e5c47331e1a335fd1327d708a29ef6908d6beb37
Connor <cwg1231@gmail.com>
2024-04-18 21:31:23 -0700

    Removed deprecated help function

--- help.py
+++ help.py
@@ -4,43 +4,6 @@
 
 from textwrap import wrap 
 
-# print_help command is deprecated 
-def print_help():
-    # List of valid commands
-    print("help, calc, exit, list_ingredients, load_ingredients, list_recipes, load_recipes")
-    print()
-
-    # Command descriptions
-    print("help")
-    print("\t List commands and their usage.")
-    print()
-
-    print("calc <recipe> <quantity>")
-    print("\t Given a recipe and quantity, calculate the required quantity of ")
-    print("\t ingredients for each.")
-    print()
-
-    print("list_ingredients")
-    print("\t List all the ingredients that are currently loaded from data sets.")
-    print()
-
-    print("load_ingredients <file>")
-    print("\t Load ingredients from a file into the session.")
-    print()
-
-    print("list_recipes")
-    print("\t List all the recipes that are currently loaded from data sets.")
-    print()
-
-    print("load_recipes <file>")
-    print("\t Load recipes from a file into the session")
-    print()
-
-    print("exit")
-    print("\t Exit quartermaster.")
-    print()
-
-
 def print_help_db():
 	for item in help_database:
 		lines = help_database[item].split("\n")
