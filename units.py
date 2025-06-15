a0018977516114960fec3c773bf883621383047e
sudo-nano <cwg1231@gmail.com>
2025-06-14 21:54:45 -0700

    Created VolumeUnit enum

--- units.py
+++ units.py
@@ -1,5 +1,6 @@
 # Quartermaster Unit Conversions
 from enum import Enum
+from math import isnan
 
 class TempUnit(Enum):
     celsius = 0
@@ -9,31 +10,30 @@ class TempUnit(Enum):
 
 class Temperature:
     # Temperature is internally stored in Celsius.
-    temp_celsius = 0
-
     def __init__(self, temp, unit):
         self.set_temp(temp, unit)
 
     def set_temp(self, temp, unit):
         # Type check temperature value
-        temp_is_int = isinstance(temp, int)
-        temp_is_float = isinstance(temp, float)
-        if not (temp_is_int or temp_is_float):
+        if type(temp) not in [int, float]:
             raise TypeError(f"Temperature value is invalid type {type(temp)}. Use int or float instead.")
+
+        if isnan(temp):
+            raise ValueError("Provided temperature is NaN")
 
         # Type check input unit and convert to celsius
         match unit:
             case TempUnit.celsius:
-                self.intermediate_celsius = temp
+                self.temp_celsius = temp
 
             case TempUnit.fahrenheit:
-                self.intermediate_celsius = (temp - 32) * 1.8
+                self.temp_celsius = (temp - 32) * 1.8
 
             case TempUnit.kelvin:
-                self.intermediate_celsius = temp - 273.15
+                self.temp_celsius = temp - 273.15
 
             case TempUnit.rankine:
-                self.intermediate_celsius = (temp * 1.8) - 273.15
+                self.temp_celsius = (temp * 1.8) - 273.15
 
             case other:
                 raise TypeError(f"Cannot convert from invalid temperature unit {unit}. See TempUnit enum for valid units.")
@@ -56,3 +56,22 @@ class Temperature:
 
             case other:
                 raise TypeError(f"Cannot convert to invalid temperature unit {unit_to}. See TempUnit enum for valid units.")
+
+# This is so evil
+# https://en.wikipedia.org/wiki/Comparison_of_the_imperial_and_US_customary_measurement_systems
+class VolumeUnit(Enum):
+    milliliters = 0
+    liters = 1
+    fl_oz_imperial = 2
+    fl_oz_customary = 3
+    pint_imperial = 4
+    pint_customary_liquid = 5
+    pint_customary_dry = 6
+    quart_imperial = 7
+    quart_customary_liquid = 8
+    quart_customary_dry = 9
+
+    # For some reason gallons in US customary units do not differentiate
+    # between liquid and dry measure, even though pint and quart do
+    gallon_imperial = 10
+    gallon_customary = 11
